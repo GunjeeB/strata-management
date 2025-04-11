a104279527cc72baec32217da71e93fec12af6bc
Gunjee Gunjee <gunjeegunjee@Gunjees-MacBook-Air.local>
2025-04-12 00:46:47 +1000

    changed mainn

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import styles from '../styles/Home.module.css';
-import styles from '../styles/Home.module.css';
 
 export default function Home() {
   const [activeSection, setActiveSection] = useState('home');
